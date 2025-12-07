7c3d813582ff258580107618995d492bb7769ec7
Delory Louise <delory.louise@gmail.com>
2025-12-07 12:41:21 +0000

    Finalisation de l'application (test hors posit cloud).

--- application-examen.R
+++ application-examen.R
@@ -3,54 +3,57 @@ library(ggplot2)
 library(dplyr)
 library(DT)
 library(bslib)
+library(ggplot2)
+library(plotly)
+
 
 thematic::thematic_shiny(font = "auto")
 
 ui <- fluidPage(
   theme = bs_theme(
     version = 5,
-    bootswatch = "minty"),
+    bootswatch = "minty" ),
   
   titlePanel("Exploration des Diamants"),
-
+  
   sidebarLayout(
     
     sidebarPanel(
       radioButtons(
         inputId = "filtrage", 
-        label="Colorier les points en rose ?", 
-        choices = list("Oui"=1, "Non"=2), 
+        label = "Colorier les points en rose ?",
+        choices = list("Oui" = 1, "Non" = 2), 
         selected = 1
       ),
       
       selectInput(
         inputId = "couleur",
-        label = "Choisir une couleur à filtrer",
-        choices = c("D","E", "F", "G", "H", "I", "J"),
+        label = "Choisir une couleur à filtrer :",
+        choices = c("D","E","H","I","J"),
         selected = "J"
       ),
       
       sliderInput(
         inputId = "prix",
-        label = "Prix maximum",
+        label = "Prix maximum :",
         min = 300,
         max = 20000,
-        value = 3000), 
+        value = 3000
+      ),
       
-      actionButton(
-        inputId = "boutton", 
-        label= "Visualiser le graphe"
+      actionButton( 
+        inputId = "boutton",
+        label = "Visualiser le graphe"
       )
     ),
     
     mainPanel(
       textOutput(outputId = "DiamondsText"),
-      plotOutput(outputId = "DiamondsPolts"),
+      plotlyOutput(outputId = "DiamondsPlots"),
       DT::DTOutput(outputId = "DiamondsTableau")
     )
   )
 )
-
 
 
 server <- function(input, output) {
@@ -59,15 +62,15 @@ server <- function(input, output) {
   
   observeEvent(input$boutton, {
     
-    message(showNotification(
-      paste("Prix", inputId$prix, "& Color", inputId$couleur),
+    showNotification(
+      paste("Prix", input$prix, "& Color", input$couleur),
       type = "message"
-    ))
+    )
     
-    rv$str <- diamonds |> 
-      filter(price > input$prix & color == input$couleur)
+    rv$filtre <- diamonds %>% 
+      filter(price < input$prix & color == input$couleur)  
     
-    rv$couleur_points <- if (input$filtrage ==1) "pink" else "black"
+    rv$couleur_points <- if (input$filtrage == 1) "pink" else "black"
     
     rv$graphique <- 
       ggplot(rv$filtre, aes(price, carat)) + 
@@ -77,31 +80,15 @@ server <- function(input, output) {
       )
   })
   
-  
-  output$StarWarsPlot <- renderPlot({
-    rv$plo
+  output$DiamondsPlots <- plotly::renderPlotly({
+    req(rv$graphique)
+    ggplotly(rv$graphique)
   })
   
-  #output$StarWarsPlot <- renderPlot({
-  #if (is.null(graphs$plot)) {
-  #  starwars
-  #}
-  #})
-  
-  output$textstarwars <- renderText ({
-    nombre_lignes <- rv$str |>
-      nrow() 
-    
-    paste("Nb lignes :", nombre_lignes)
-  })
-  
-  output$tableau <- DT::renderDT({
-    rv$str
+  output$DiamondsTableau <- DT::renderDT({
+    rv$filtre
   })
 }
 
-
-
-
-#Run the application
+# Run the application
 shinyApp(ui = ui, server = server)
